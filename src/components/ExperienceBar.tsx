40207336283f226d8ea5a442d536c84ea86e48e2
Maycon <maycondsn@gmail.com>
2021-02-23 14:54:45 -0400

    Day 01 - Next Level Week

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,15 @@
+export function ExperienceBar() {
+    return (
+        <header className="experience-bar">
+            <span>0xp</span>
+            <div>
+                <div style={{width: '60%'}} />
+
+                <span className="current-experience" style={{left: "50%"}}>
+                    300px
+                </span>
+            </div>
+            <span>600xp</span>
+        </header>
+    )
+}
